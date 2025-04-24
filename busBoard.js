7341851b46820712fcc51c3646eb0647c3e56041
Winnie Kwok <winnie.kwok@softwire.com>
2025-04-24 14:05:33 +0100

    Sorted bus list

--- busBoard.js
+++ busBoard.js
@@ -8,9 +8,13 @@ const api_key = process.env.api_key;
 async function getArrivalsFromTfl(stopPointId) {
     const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopPointId}/Arrivals?api_key=${api_key}`);
     const responseBody = await response.json();
-    return responseBody;
+
+    let sortedResponse = responseBody.sort((a, b)=>a["timeToStation"]-b["timeToStation"]);
+
+    return sortedResponse;
 };
 
-await getArrivalsFromTfl('490008660N');
+console.log(await getArrivalsFromTfl('490008660N'));
 
-//const lineName = (await getArrivalsFromTfl('490008660N'))[0].lineName;
+//await getArrivalsFromTfl('490008660N');
+
