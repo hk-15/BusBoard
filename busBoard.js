65c17e2eed4136df8f628365502b5a3ac1935ad9
Sathya Sreenivasan <sathya.sreenivasan@softwire.com>
2025-04-24 16:02:33 +0100

    Retrieving postcode api draft

--- busBoard.js
+++ busBoard.js
@@ -10,20 +10,29 @@ const api_key = process.env.api_key;
 async function getArrivalsFromTfl(stopPointId) {
     const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopPointId}/Arrivals?api_key=${api_key}`);
     const responseBody = await response.json();
-
     let sortedResponse = responseBody.sort((a, b)=>a["timeToStation"]-b["timeToStation"]);
-    
+     
     return sortedResponse;
 };
 
-//console.log(await getArrivalsFromTfl('490008660N'));
-const input = prompt("Enter the stop point Id : ");
+async function getPostCodeData(postCode) {
+    const responsePostCode = await fetch(`https://api.postcodes.io/${postCode}`);
+    const responsePostCodeBody = await responsePostCode.json();
+    //console.log(responsePostCodeBody);
+    return responsePostCodeBody;
+};
 
-let sortedList = await getArrivalsFromTfl(input);
+
+
+
+const input = prompt("Enter the post code : ");
+console.log(await getPostCodeData('490008660N'));
+
+/*let sortedList = await getArrivalsFromTfl(input);
 for (let i = 0; i < 5; i++) {
     console.log("Line Name: " + sortedList[i].lineName);
     console.log("Minutes to arrive at the Station: " + Math.round(sortedList[i].timeToStation/60));
     console.log("Destination: " + sortedList[i].destinationName);
     console.log("---------------------------------------------------------------------------------------------");
-}
+}*/
 
